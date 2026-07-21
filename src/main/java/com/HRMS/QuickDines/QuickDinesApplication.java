d8614e75065641c9889a4676a178171c2d46f472
Saisree Andavarapu <saisreeandavarapu@gmail.com>
2026-07-21 10:48:17 +0530

    first commit

---
+++ src/main/java/com/HRMS/QuickDines/QuickDinesApplication.java
@@ -0,0 +1,13 @@
+package com.HRMS.QuickDines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickDinesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickDinesApplication.class, args);
+	}
+
+}
