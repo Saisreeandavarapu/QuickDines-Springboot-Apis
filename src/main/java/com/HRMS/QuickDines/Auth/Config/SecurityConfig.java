572d99f888ffe75de24eed60ba2304ba37700846
Saisree Andavarapu <saisreeandavarapu@gmail.com>
2026-07-31 10:59:45 +0530

    add auth file

--- src/main/java/com/HRMS/QuickDines/Auth/Config/SecurityConfig.java
+++ src/main/java/com/HRMS/QuickDines/Auth/Config/SecurityConfig.java
@@ -65,9 +65,13 @@ public class SecurityConfig{
                 .sessionManagement(session->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth-> auth
-                        .requestMatchers("/api/auth/**")
-                        .permitAll()
-                        .anyRequest().authenticated())
+                                .requestMatchers(
+                                        "/",
+                                        "/error",
+                                        "/api/auth/**"
+                                ).permitAll()
+                                .anyRequest().authenticated()
+                        )
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
 
