03f75b830d516420a06154e423c84b7730993ab2
Saisree Andavarapu <saisreeandavarapu@gmail.com>
2026-08-21 14:49:52 +0530

    add Event file

--- src/main/java/com/HRMS/QuickDines/Auth/Config/SecurityConfig.java
+++ src/main/java/com/HRMS/QuickDines/Auth/Config/SecurityConfig.java
@@ -1,140 +1,140 @@
-package com.HRMS.QuickDines.Auth.Config;
-
-import com.HRMS.QuickDines.Auth.Config.JwtAuthenticationFilter;
-import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-@Configuration
-@EnableWebSecurity
-@RequiredArgsConstructor
-public class SecurityConfig {
-
-
-    private final JwtAuthenticationFilter jwtFilter;
-
-    private final UserDetailsService userDetailsService;
-
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-
-        return new BCryptPasswordEncoder();
-
-    }
-
-
-    @Bean
-    public AuthenticationProvider authenticationProvider() {
-
-        DaoAuthenticationProvider provider =
-                new DaoAuthenticationProvider(userDetailsService);
-
-        provider.setPasswordEncoder(passwordEncoder());
-
-        return provider;
-    }
-
-
-    @Bean
-    public AuthenticationManager authenticationManager(
-            AuthenticationConfiguration configuration) throws Exception {
-        return configuration.getAuthenticationManager();
-    }
-
-
+//package com.HRMS.QuickDines.Auth.Config;
+//
+//import com.HRMS.QuickDines.Auth.Config.JwtAuthenticationFilter;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.authentication.AuthenticationManager;
+//import org.springframework.security.authentication.AuthenticationProvider;
+//import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+//import org.springframework.security.config.Customizer;
+//import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.http.SessionCreationPolicy;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.security.web.SecurityFilterChain;
+//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+//
+//@Configuration
+//@EnableWebSecurity
+//@RequiredArgsConstructor
+//public class SecurityConfig {
+//
+//
+//    private final JwtAuthenticationFilter jwtFilter;
+//
+//    private final UserDetailsService userDetailsService;
+//
+//
 //    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http)throws Exception {
-//        http.csrf(csrf -> csrf.disable())
+//    public PasswordEncoder passwordEncoder() {
+//
+//        return new BCryptPasswordEncoder();
+//
+//    }
+//
+//
+//    @Bean
+//    public AuthenticationProvider authenticationProvider() {
+//
+//        DaoAuthenticationProvider provider =
+//                new DaoAuthenticationProvider(userDetailsService);
+//
+//        provider.setPasswordEncoder(passwordEncoder());
+//
+//        return provider;
+//    }
+//
+//
+//    @Bean
+//    public AuthenticationManager authenticationManager(
+//            AuthenticationConfiguration configuration) throws Exception {
+//        return configuration.getAuthenticationManager();
+//    }
+//
+//
+////    @Bean
+////    public SecurityFilterChain securityFilterChain(HttpSecurity http)throws Exception {
+////        http.csrf(csrf -> csrf.disable())
+////                .cors(Customizer.withDefaults())
+////                .authenticationProvider(authenticationProvider())
+////                .sessionManagement(session ->
+////                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+////                .authorizeHttpRequests(auth -> auth
+////                        .requestMatchers(
+////                                "/",
+////                                "/error",
+////                                "/api/auth/**")
+////                        .permitAll()
+////                        .anyRequest().authenticated()
+////                )
+////                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+////        return http.build();
+////    }
+//
+////@Bean
+////public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+////
+////    http
+////            .csrf(csrf -> csrf.disable())
+////
+////            .cors(Customizer.withDefaults())
+////
+////            .authenticationProvider(authenticationProvider())
+////
+////            .sessionManagement(session ->
+////                    session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+////            )
+////
+////            .authorizeHttpRequests(auth -> auth
+////                    .anyRequest().permitAll()
+////            );
+////
+////    return http.build();
+////}
+////
+////
+////    }
+//
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(
+//            HttpSecurity http) throws Exception {
+//
+//        http
+//                .csrf(csrf -> csrf.disable())
+//
 //                .cors(Customizer.withDefaults())
-//                .authenticationProvider(authenticationProvider())
+//
+//                // IMPORTANT:
+//                // Do NOT use STATELESS
+//                // We want HttpSession authentication
 //                .sessionManagement(session ->
-//                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+//                        session
+//                                .sessionCreationPolicy(
+//                                        org.springframework.security.config.http
+//                                                .SessionCreationPolicy.IF_REQUIRED
+//                                )
+//                )
+//
 //                .authorizeHttpRequests(auth -> auth
+//
+//                        // Public APIs
 //                        .requestMatchers(
 //                                "/",
 //                                "/error",
-//                                "/api/auth/**")
-//                        .permitAll()
+//                                "/api/auth/**"
+//                        ).permitAll()
+//
+//                        // Everything else requires login
 //                        .anyRequest().authenticated()
-//                )
-//                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+//                );
+//
 //        return http.build();
 //    }
-
-//@Bean
-//public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//
-//    http
-//            .csrf(csrf -> csrf.disable())
-//
-//            .cors(Customizer.withDefaults())
-//
-//            .authenticationProvider(authenticationProvider())
-//
-//            .sessionManagement(session ->
-//                    session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-//            )
-//
-//            .authorizeHttpRequests(auth -> auth
-//                    .anyRequest().permitAll()
-//            );
-//
-//    return http.build();
 //}
 //
 //
-//    }
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(
-            HttpSecurity http) throws Exception {
-
-        http
-                .csrf(csrf -> csrf.disable())
-
-                .cors(Customizer.withDefaults())
-
-                // IMPORTANT:
-                // Do NOT use STATELESS
-                // We want HttpSession authentication
-                .sessionManagement(session ->
-                        session
-                                .sessionCreationPolicy(
-                                        org.springframework.security.config.http
-                                                .SessionCreationPolicy.IF_REQUIRED
-                                )
-                )
-
-                .authorizeHttpRequests(auth -> auth
-
-                        // Public APIs
-                        .requestMatchers(
-                                "/",
-                                "/error",
-                                "/api/auth/**"
-                        ).permitAll()
-
-                        // Everything else requires login
-                        .anyRequest().authenticated()
-                );
-
-        return http.build();
-    }
-}
-
-
-
+//
